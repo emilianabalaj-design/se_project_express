162f21fe5d8471a405581b3812941955097a9855
EMILIANA BALAJ <EMILIANABALAJ@GMAIL.COM>
2026-03-04 13:57:20 -0500

    fix eslint config

--- controllers/users.js
+++ controllers/users.js
@@ -6,9 +6,7 @@ const getUsers = (req, res) => {
   User.find({})
     .orFail()
     .then((users) => res.status(200).send(users))
-    .catch((err) => {
-      return res.status(500).send({ message: err.message });
-    });
+    .catch((err) => res.status(500).send({ message: err.message }));
 };
 
 const createUser = (req, res) => {

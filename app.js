b1dcd1ba40a069bb514b4f6cd24ec6ba79a50fb7
EMILIANA BALAJ <EMILIANABALAJ@GMAIL.COM>
2026-03-09 15:52:18 -0400

    fix: remove duplicate routes and update error middleware

--- app.js
+++ app.js
@@ -27,13 +27,11 @@ app.post("/signup", createUser);
 
 app.use(auth);
 
-app.use("/users", usersRouter);
-app.use("/items", clothingItemsRouter);
-
 app.use(routes);
 
-app.use((err, req, res) => {
+app.use((err, req, res, next) => {
   const { statusCode = 500, message } = err;
+
   res.status(statusCode).send({
     message: statusCode === 500 ? "An error occurred" : message,
   });
